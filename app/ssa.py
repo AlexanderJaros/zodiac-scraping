5d233665e02901c20db77052d8d07e81879eb478
AlexanderJaros <59668912+AlexanderJaros@users.noreply.github.com>
2020-05-05 16:02:14 -0400

    docstring

--- app/ssa.py
+++ app/ssa.py
@@ -10,13 +10,24 @@ import requests
 #    return soup
 
 def ssa_scrape(name, sex): 
+    """
+    Takes user inputted baby name and sex and insert into Social Security Admin website form to retrieve name popularity data.
+    
+    Params:
+        Name (string); chosen first name of babay
+        Sex (string); abbreivated Male as "M" or Female as "F"
+    
+    Examples:
+        ssa_scrape("John", "M")
+        ssa_scrape("Sarah", "F")
+    """
     while True:
         request_url = "https://www.ssa.gov/cgi-bin/babyname.cgi"
         params = {"name": name, "sex": sex}
         response = requests.post(request_url, params)
         soup = BeautifulSoup(response.text, "html.parser")
         if not "Please enter another name." in soup.text:
-            facts = soup.body.find("ul")
+            facts = soup.body.find("ul").find("li")
             return facts.text
         else:
             return "Heg is not in the top 1000 names for any year of birth beginning with 2000. Please enter another name."
